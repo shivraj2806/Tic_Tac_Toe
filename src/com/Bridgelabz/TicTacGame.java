0e07e7a81218d2e140dbc632b5807d3f2b008165
shivraj2806 <shivjadhav2806@gmail.com>
2021-09-29 22:45:45 +0530

    Added: creating display board

--- src/com/Bridgelabz/TicTacGame.java
+++ src/com/Bridgelabz/TicTacGame.java
@@ -4,31 +4,40 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class TicTacGame {
-        public static char playerChoice;
-        public static char computerChoice;
+    public static char playerChoice;
+    public static char computerChoice;
 
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-        }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        public static void initialisingBoard() {
-            char[] emptyBoardCells = new char[10];
-            for (int i = 0; i < 10; i++) {
-                emptyBoardCells[i] = ' ';
-            }
-        }
+        displayBoard(initialisingBoard());
+    }
 
-        public static void chooseLetter(Scanner scanner) {
-            System.out.println("Which one you want go with ?\n" +
-                    "'X' or 'O'");
-            while (true) {
-                char letter = scanner.next().toUpperCase(Locale.ROOT).charAt(0);
-                if (letter == 'X' || letter == 'O') {
-                    playerChoice = letter;
-                    computerChoice = (letter == 'X') ? 'O' : 'X';
-                    break;
-                } else
-                    System.out.println("INVALID INPUT!!!");
-            }
+    public static char[] initialisingBoard() {
+        char[] emptyBoardCells = new char[10];
+        for (int i = 0; i < 10; i++)
+            emptyBoardCells[i] = '-';
+
+        return emptyBoardCells;
+    }
+
+    public static void chooseLetter(Scanner scanner) {
+        System.out.println("Which one you want go with ?\n" +
+                "'X' or 'O'");
+        while (true) {
+            char letter = scanner.next().toUpperCase(Locale.ROOT).charAt(0);
+            if (letter == 'X' || letter == 'O') {
+                playerChoice = letter;
+                computerChoice = (letter == 'X') ? 'O' : 'X';
+                break;
+            } else
+                System.out.println("INVALID INPUT!!!");
         }
     }
+
+    public static void displayBoard(char[] BoardCells) {
+        System.out.println(BoardCells[1] + "|" + BoardCells[2] + "|" + BoardCells[3]
+                + "\n" + BoardCells[4] + "|" + BoardCells[5] + "|" + BoardCells[6]
+                + "\n" + BoardCells[7] + "|" + BoardCells[8] + "|" + BoardCells[9]);
+    }
+}

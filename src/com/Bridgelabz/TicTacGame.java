b895a6decf992e07f82a146a54a2d39f4c5f0e64
shivraj2806 <shivjadhav2806@gmail.com>
2021-09-29 22:41:35 +0530

    Added: choosing X or O

--- src/com/Bridgelabz/TicTacGame.java
+++ src/com/Bridgelabz/TicTacGame.java
@@ -1,13 +1,34 @@
 package com.Bridgelabz;
 
-public class TicTacGame {
-    public static void main(String[] args) {
-    }
+import java.util.Locale;
+import java.util.Scanner;
 
-    public static void initialisingBoard() {
-        char[] emptyBoardCells = new char[10];
-        for (int i = 0; i < 10; i++) {
-            emptyBoardCells[i] = ' ';
+public class TicTacGame {
+        public static char playerChoice;
+        public static char computerChoice;
+
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+        }
+
+        public static void initialisingBoard() {
+            char[] emptyBoardCells = new char[10];
+            for (int i = 0; i < 10; i++) {
+                emptyBoardCells[i] = ' ';
+            }
+        }
+
+        public static void chooseLetter(Scanner scanner) {
+            System.out.println("Which one you want go with ?\n" +
+                    "'X' or 'O'");
+            while (true) {
+                char letter = scanner.next().toUpperCase(Locale.ROOT).charAt(0);
+                if (letter == 'X' || letter == 'O') {
+                    playerChoice = letter;
+                    computerChoice = (letter == 'X') ? 'O' : 'X';
+                    break;
+                } else
+                    System.out.println("INVALID INPUT!!!");
+            }
         }
     }
-}
